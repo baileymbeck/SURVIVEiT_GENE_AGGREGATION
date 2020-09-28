fa63f3347c91bfd09a2632d0b4c420617a449375
Bailey Beck <baileymbeck@gmail.com>
2020-09-28 17:36:31 -0500

    sudo code objectives

--- Webscrape_Expertscape.py
+++ Webscrape_Expertscape.py
@@ -2,12 +2,26 @@ import requests
 from bs4 import BeautifulSoup as bs
 from csv import writer
 
+# connection to Expertscape site
 r = requests.get('https://expertscape.com/ex/urinary%20bladder%20neoplasms')
 
+# initialize beautiful soup 
 soup = bs(r.content, 'html.parser')
 
+# below allows easier readability, saving script here for future pages
 # print(soup.prettify())
 
+# this is the column name doctor list
 doctor_column = soup.find('td', attrs={'id': 'expColTD_a'})
+
+# ToDo 
+# find a way to loop through list
+# select function
+# name
+# ranking
+# specialty
+# 3 locations
+# Nice To Haves
+# key word search for gene type
 
 print(doctor_column)
